6d1f3699b700c58c0d56b33badcc7369a45347cb
yyn <934325265@qq.com>
2018-09-20 10:37:47 +0800

    ''

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField, StringField, SelectField
